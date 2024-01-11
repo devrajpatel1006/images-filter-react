25b3b0aca57d8d72ad7ec78f0f3bda59cd2a4083
devrajpatel1006 <devrajpatel1006@gmail.com>
2024-01-11 22:17:15 +0530

    updated evn vars

--- src/utilities/supabase.js
+++ src/utilities/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Replace 'YOUR_API_KEY' and 'YOUR_PROJECT_URL' with your actual API key and project URL
-const supabase = createClient(import.meta.env.SUPABASE_URL, import.meta.env.SUPABASE_SECRET);
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_SECRET);
 
 export default supabase;
